4d87e2ba0bb649b07085a9cad7f6d6e5be8fad62
Nickzjz <zhang.jinzh@northeastern.edu>
2024-09-17 19:48:22 -0400

    ScenceCreated

---
+++ Project1/Assets/Scripts/LightTriger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTriger : MonoBehaviour
+{
+
+    public GameObject light1; // 特定灯光 1
+    public GameObject light2; // 特定灯光 2
+    public GameObject BossLight1; 
+    public GameObject BossLight2; 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // 检查是否是玩家
+        {
+            light1.SetActive(true); 
+            light2.SetActive(true);
+            BossLight1.SetActive(true);
+            BossLight2.SetActive(true);
+
+        }
+    }
+}
