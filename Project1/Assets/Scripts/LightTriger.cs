3186779445648e5c6d2012ff62fa825616adf374
Nickzjz <zhang.jinzh@northeastern.edu>
2024-09-17 23:35:17 -0400

    地编draft2

--- Project1/Assets/Scripts/LightTriger.cs
+++ Project1/Assets/Scripts/LightTriger.cs
@@ -8,7 +8,15 @@ public class LightTriger : MonoBehaviour
     public GameObject light1; // 特定灯光 1
     public GameObject light2; // 特定灯光 2
     public GameObject BossLight1; 
-    public GameObject BossLight2; 
+    public GameObject BossLight2;
+
+    private void Start()
+    {
+        light1.SetActive(false);
+        light2.SetActive(false);
+        BossLight1.SetActive(false);
+        BossLight2.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) // 检查是否是玩家
