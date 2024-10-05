9d6e1724a5044ad5197893bfe11aeecf9ef452d1
Wesily-G <729009583@qq.com>
2024-10-05 16:20:17 -0400

    project4_initial

---
+++ Project4/Assets/Scripts/CharacterFunction/CharacterCamera.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tools;
+
+public class CharacterCamera : Singleton<CharacterCamera>
+{
+
+    public float xsen = 1, ysen = 1;
+    public Transform targetCamera;
+    [Range(0,180)]
+    public float maxPitch = 60;
+    private float pitch = 0;
+     
+    public Texture2D cursorTex;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        if (cursorTex)
+        {
+            // para2: texture offset position
+            Cursor.SetCursor(cursorTex,Vector2.zero,CursorMode.Auto);
+        }
+        
+    }
+
+
+    void Update()
+    {
+        var curEuler = targetCamera.localRotation.eulerAngles;
+        curEuler = new Vector3(pitch - Input.GetAxis("Mouse Y"), curEuler.y + Input.GetAxis("Mouse X"), 0);
+        curEuler.z = 0;
+        curEuler.x = Mathf.Clamp(curEuler.x, -maxPitch, maxPitch);
+        pitch = curEuler.x;
+        targetCamera.localRotation = Quaternion.Euler(curEuler);
+    }
+}
---
+++ Project4/Assets/Scripts/Mangers/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using Tools;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    [DisallowNull]
+    [SerializeField] private AudioSource playerAudioSource;
+    private bool isMoving=false;
+    
+    private void Update()
+    {
+        // player character movement audio
+        if(playerAudioSource.gameObject.TryGetComponent<CharacterPhysicalMovement>(out CharacterPhysicalMovement cpm))
+            isMoving = cpm.isMoving;
+        if (isMoving)
+        {
+            if (!playerAudioSource.isPlaying) playerAudioSource.Play(0);
+        }else
+            playerAudioSource.Pause();
+    }
+
+    public IEnumerator AwakeSound(AudioSource audioSource,float targetVolume,float startVolume, float speedPerPointSecond)
+    {
+        audioSource.volume = startVolume;
+        audioSource.Play(0);
+        while (audioSource.volume < targetVolume)
+        {
+            audioSource.volume += speedPerPointSecond;
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }   
+    
+    public IEnumerator ExtinguishSound(AudioSource audioSource,float targetVolume,float startVolume, float speedPerPointSecond)
+    {
+        audioSource.volume = startVolume;
+        audioSource.Play(0);
+        while (audioSource.volume > targetVolume)
+        {
+            audioSource.volume -= speedPerPointSecond;
+            yield return new WaitForSeconds(0.1f);
+        }
+        
+    }   
+    
+}
