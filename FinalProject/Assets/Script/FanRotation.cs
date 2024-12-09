aaee1f8bf19f5fb1b3e6411dca215eb9ded4fffd
Wesily-G <729009583@qq.com>
2024-12-09 17:57:29 -0500

    update

---
+++ FinalProject/Assets/Script/FanRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+    public float rotationSpeed = 50f; // 旋转速度
+
+    void Update()
+    {
+        // 每帧旋转扇叶
+        transform.Rotate(new Vector3(0,1,0), rotationSpeed * Time.deltaTime);
+    }
+}
